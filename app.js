a2bc0499873c1dfe475dd3a1034dd8440e7c0ef4
Mark <duffy_mark@hotmail.com>
2022-02-04 15:51:00 -0400

    added to resume

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ displayDot();
 console.log('Thank you for visiting my website')
 console.log('If you would like to collaborate together please don\'t hesitate to reach out to me')
 console.log('Click my photo at the top of the page!')
+console.log('Click the Front-End Developer text too')
 
 // TEST
 
